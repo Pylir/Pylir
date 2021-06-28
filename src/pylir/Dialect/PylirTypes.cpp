7946f5d3010beb3c784a61ef5b748380a881f8eb
zero9178 <markus.boeck02@gmail.com>
2021-06-28 21:41:26 +0200

    Custom parse and print for pylir.variant type

--- src/pylir/Dialect/PylirTypes.cpp
+++ src/pylir/Dialect/PylirTypes.cpp
@@ -1,8 +1,47 @@
 
 #include "PylirTypes.hpp"
 
-#include <llvm/ADT/TypeSwitch.h>
 #include <mlir/IR/DialectImplementation.h>
+
+#include <llvm/ADT/TypeSwitch.h>
 
 #define GET_TYPEDEF_CLASSES
 #include "pylir/Dialect/PylirOpsTypes.cpp.inc"
+
+mlir::Type pylir::Dialect::PylirVariantType::parse(::mlir::MLIRContext* context, ::mlir::DialectAsmParser& parser)
+{
+    if (parser.parseLess())
+    {
+        return {};
+    }
+    llvm::SmallVector<mlir::Type> containedTypes;
+    {
+        Type containedType;
+        if (parser.parseType(containedType))
+        {
+            return {};
+        }
+        containedTypes.push_back(std::move(containedType));
+    }
+    while (!parser.parseOptionalComma())
+    {
+        Type containedType;
+        if (parser.parseType(containedType))
+        {
+            return {};
+        }
+        containedTypes.push_back(std::move(containedType));
+    }
+    if (parser.parseGreater())
+    {
+        return {};
+    }
+    return PylirVariantType::get(context, containedTypes);
+}
+
+void pylir::Dialect::PylirVariantType::print(::mlir::DialectAsmPrinter& printer) const
+{
+    printer << "tuple<";
+    llvm::interleaveComma(getTypes(), printer);
+    printer << ">";
+}
