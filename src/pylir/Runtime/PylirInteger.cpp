1b9812323c59f44cfe63dc0f8a11f77776d63fcc
zero9178 <markus.boeck02@gmail.com>
2021-08-17 09:17:42 +0200

    Simple stop gap solution for integers using a single 64 integer put into the pointer

--- src/pylir/Runtime/PylirInteger.cpp
+++ src/pylir/Runtime/PylirInteger.cpp
@@ -1,11 +1,13 @@
 #include "Pylir.h"
 
+// TODO Big Integer
+
 PylirIntegerValue pylir_integer_from_size_t(size_t value)
 {
-    return {};
+    return {reinterpret_cast<void*>(value)};
 }
 
 PylirIntegerValue pylir_integer_mul(PylirIntegerValue lhs, PylirIntegerValue rhs)
 {
-    return {};
+    return {reinterpret_cast<void*>(reinterpret_cast<size_t>(lhs.opaque) * reinterpret_cast<size_t>(rhs.opaque))};
 }
