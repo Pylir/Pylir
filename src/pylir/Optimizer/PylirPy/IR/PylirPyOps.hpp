d35e4a6d9c203cf7dd04969c71935545c4f30a84
zero9178 <markus.boeck02@gmail.com>
2021-09-16 16:46:10 +0200

    Fix missing include

--- src/pylir/Optimizer/PylirPy/IR/PylirPyOps.hpp
+++ src/pylir/Optimizer/PylirPy/IR/PylirPyOps.hpp
@@ -3,6 +3,7 @@
 
 #include <mlir/IR/OpDefinition.h>
 #include <mlir/IR/OpImplementation.h>
+#include <mlir/IR/SymbolTable.h>
 #include <mlir/Interfaces/InferTypeOpInterface.h>
 #include <mlir/Interfaces/SideEffectInterfaces.h>
 
