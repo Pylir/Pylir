41be43188e48ca79c80ade551bb0a2509edf29ac
zero9178 <markus.boeck02@gmail.com>
2021-11-17 20:49:18 +0100

    Make call.py test more robust

--- test/CodeGen/call.py
+++ test/CodeGen/call.py
@@ -7,27 +7,52 @@ global x
 x()
 
 # CHECK: %[[X_LOADED:.*]] = py.load @x
-# CHECK: %[[TUPLE:.*]] = py.makeTuple ()
-# CHECK: %[[DICT:.*]] = py.constant #py.dict<{}>
-# CHECK: call_indirect %{{.*}}(%{{.*}}, %[[TUPLE]], %[[DICT]])
+# CHECK: %[[IS_UNBOUND:.*]] = py.isUnboundValue %[[X_LOADED]]
+# CHECK: cond_br %[[IS_UNBOUND]], ^{{[[:alnum:]]+}}, ^[[HAPPY_PATH:[[:alnum:]]+]]
+
+# CHECK: ^[[HAPPY_PATH]]:
+# CHECK-DAG: %[[TUPLE:.*]] = py.makeTuple ()
+# CHECK-DAG: %[[DICT:.*]] = py.constant #py.dict<{}>
+# CHECK: %[[RESULT:.*]], %[[SUCCESS:.*]] = py.getFunction %[[X_LOADED]]
+# CHECK: cond_br %[[SUCCESS]], ^[[HAPPY_PATH:[[:alnum:]]+]]
+
+# CHECK: ^[[HAPPY_PATH]]:
+# CHECK: %[[FUNCTION:.*]] = py.function.getFunction %[[RESULT]]
+# CHECK: call_indirect %[[FUNCTION]](%[[RESULT]], %[[TUPLE]], %[[DICT]])
 
 x(5, k=3)
 
 # CHECK: %[[X_LOADED:.*]] = py.load @x
+# CHECK: %[[IS_UNBOUND:.*]] = py.isUnboundValue %[[X_LOADED]]
+# CHECK: cond_br %[[IS_UNBOUND]], ^{{[[:alnum:]]+}}, ^[[HAPPY_PATH:[[:alnum:]]+]]
+
+# CHECK: ^[[HAPPY_PATH]]:
 # CHECK: %[[FIVE:.*]] = py.constant #py.int<5>
 # CHECK: %[[K:.*]] = py.constant "k"
 # CHECK: %[[THREE:.*]] = py.constant #py.int<3>
 # CHECK-DAG: %[[TUPLE:.*]] = py.makeTuple (%[[FIVE]])
 # CHECK-DAG: %[[DICT:.*]] = py.makeDict (%[[K]] : %[[THREE]])
-# ... mro lookup
-# CHECK: call_indirect %{{.*}}(%{{.*}}, %[[TUPLE]], %[[DICT]])
+# CHECK: %[[RESULT:.*]], %[[SUCCESS:.*]] = py.getFunction %[[X_LOADED]]
+# CHECK: cond_br %[[SUCCESS]], ^[[HAPPY_PATH:[[:alnum:]]+]]
+
+# CHECK: ^[[HAPPY_PATH]]:
+# CHECK: %[[FUNCTION:.*]] = py.function.getFunction %[[RESULT]]
+# CHECK: call_indirect %[[FUNCTION]](%[[RESULT]], %[[TUPLE]], %[[DICT]])
 
 x(*(), **{})
 
 # CHECK: %[[X_LOADED:.*]] = py.load @x
+# CHECK: %[[IS_UNBOUND:.*]] = py.isUnboundValue %[[X_LOADED]]
+# CHECK: cond_br %[[IS_UNBOUND]], ^{{[[:alnum:]]+}}, ^[[HAPPY_PATH:[[:alnum:]]+]]
+
+# CHECK: ^[[HAPPY_PATH]]:
 # CHECK: %[[ARG1:.*]] = py.makeTuple ()
 # CHECK: %[[ARG2:.*]] = py.makeDict ()
 # CHECK-DAG: %[[TUPLE:.*]] = py.makeTuple (*%[[ARG1]])
 # CHECK-DAG: %[[DICT:.*]] = py.makeDict (**%[[ARG2]])
-# ... mro lookup
-# CHECK: call_indirect %{{.*}}(%{{.*}}, %[[TUPLE]], %[[DICT]])
+# CHECK: %[[RESULT:.*]], %[[SUCCESS:.*]] = py.getFunction %[[X_LOADED]]
+# CHECK: cond_br %[[SUCCESS]], ^[[HAPPY_PATH:[[:alnum:]]+]]
+
+# CHECK: ^[[HAPPY_PATH]]:
+# CHECK: %[[FUNCTION:.*]] = py.function.getFunction %[[RESULT]]
+# CHECK: call_indirect %[[FUNCTION]](%[[RESULT]], %[[TUPLE]], %[[DICT]])
