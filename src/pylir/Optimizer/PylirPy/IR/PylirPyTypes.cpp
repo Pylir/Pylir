3c58ead504771b509a065ada1f97e0e6cf0475b1
zero9178 <markus.boeck02@gmail.com>
2021-11-21 18:54:47 +0100

    Update MLIR to revision 3234f2d9c1669459833e717d34c10296d78b7818

--- src/pylir/Optimizer/PylirPy/IR/PylirPyTypes.cpp
+++ src/pylir/Optimizer/PylirPy/IR/PylirPyTypes.cpp
@@ -8,20 +8,3 @@
 
 #define GET_TYPEDEF_CLASSES
 #include "pylir/Optimizer/PylirPy/IR/PylirPyOpsTypes.cpp.inc"
-
-mlir::Type pylir::Py::PylirPyDialect::parseType(::mlir::DialectAsmParser& parser) const
-{
-    llvm::StringRef keyword;
-    if (parser.parseKeyword(&keyword))
-    {
-        return {};
-    }
-    mlir::Type result;
-    (void)generatedTypeParser(parser, keyword, result);
-    return result;
-}
-
-void pylir::Py::PylirPyDialect::printType(::mlir::Type type, ::mlir::DialectAsmPrinter& os) const
-{
-    (void)generatedTypePrinter(type, os);
-}
