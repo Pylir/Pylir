a6d87b5c453a8d109b7c1e884e664098fa3e5c18
zero9178 <markus.boeck02@gmail.com>
2021-11-23 11:30:52 +0100

    Add inliner interface to PylirPy

--- src/pylir/Optimizer/PylirPy/IR/PylirPyDialect.cpp
+++ src/pylir/Optimizer/PylirPy/IR/PylirPyDialect.cpp
@@ -2,12 +2,36 @@
 #include "PylirPyDialect.hpp"
 
 #include <mlir/Dialect/StandardOps/IR/Ops.h>
+#include <mlir/Transforms/InliningUtils.h>
 
 #include "pylir/Optimizer/PylirPy/IR/PylirPyOpsDialect.cpp.inc"
 
 #include "PylirPyAttributes.hpp"
 #include "PylirPyOps.hpp"
 #include "PylirPyTypes.hpp"
+
+namespace
+{
+struct PylirPyInlinerInterface : public mlir::DialectInlinerInterface
+{
+    using mlir::DialectInlinerInterface::DialectInlinerInterface;
+
+    bool isLegalToInline(mlir::Operation*, mlir::Operation*, bool) const override
+    {
+        return true;
+    }
+
+    bool isLegalToInline(mlir::Region*, mlir::Region*, bool, mlir::BlockAndValueMapping&) const override
+    {
+        return true;
+    }
+
+    bool isLegalToInline(mlir::Operation*, mlir::Region*, bool, mlir::BlockAndValueMapping&) const override
+    {
+        return true;
+    }
+};
+} // namespace
 
 void pylir::Py::PylirPyDialect::initialize()
 {
@@ -20,6 +44,7 @@ void pylir::Py::PylirPyDialect::initialize()
 #include "pylir/Optimizer/PylirPy/IR/PylirPyOpsTypes.cpp.inc"
         >();
     initializeAttributes();
+    addInterfaces<PylirPyInlinerInterface>();
 }
 
 mlir::Operation* pylir::Py::PylirPyDialect::materializeConstant(::mlir::OpBuilder& builder, ::mlir::Attribute value,
