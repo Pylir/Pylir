0d8662080005ab9c85b5a21b0642bbf0e957b2e5
zero9178 <markus.boeck02@gmail.com>
2022-01-31 16:49:39 +0100

    Add LLVM signal handler to pylir to print a nice stacktrace and message if a crash were to occur

--- tools/pylir/main.cpp
+++ tools/pylir/main.cpp
@@ -1,6 +1,10 @@
+#include <llvm/Support/InitLLVM.h>
+
 #include <pylir/Main/PylirMain.hpp>
 
 int main(int argc, char** argv)
 {
+    llvm::InitLLVM init(argc, argv);
+    llvm::setBugReportMsg("PLEASE submit a bug report to https://github.com/zero9178/Pylir\n");
     return pylir::main(argc, argv);
 }
