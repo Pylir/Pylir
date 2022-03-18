6ec618cf5221d52f53d4b45b46699f37559ca790
zero9178 <markus.boeck02@gmail.com>
2022-03-18 10:00:32 +0100

    Add pylir-reduce tool

---
+++ tools/pylir-reduce/main.cpp
@@ -0,0 +1,25 @@
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/InitAllDialects.h>
+#include <mlir/InitAllPasses.h>
+#include <mlir/Tools/mlir-reduce/MlirReduceMain.h>
+
+#include <pylir/Optimizer/Conversion/Passes.hpp>
+#include <pylir/Optimizer/PylirMem/IR/PylirMemDialect.hpp>
+#include <pylir/Optimizer/PylirPy/IR/PylirPyDialect.hpp>
+#include <pylir/Optimizer/PylirPy/Transforms/Passes.hpp>
+#include <pylir/Optimizer/Transforms/Passes.hpp>
+
+int main(int argc, char** argv)
+{
+    mlir::registerAllPasses();
+    pylir::registerConversionPasses();
+    pylir::registerTransformPasses();
+    pylir::Py::registerTransformPasses();
+
+    mlir::DialectRegistry registry;
+    registry.insert<pylir::Mem::PylirMemDialect, pylir::Py::PylirPyDialect>();
+    mlir::registerAllDialects(registry);
+    mlir::MLIRContext context(registry);
+
+    return mlir::failed(mlir::mlirReduceMain(argc, argv, context));
+}
