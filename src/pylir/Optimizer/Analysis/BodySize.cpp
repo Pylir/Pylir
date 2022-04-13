1028c56a2cd4dd5b3bf6e0271a3f8a3e4da82f78
zero9178 <markus.boeck02@gmail.com>
2022-04-13 12:18:40 +0200

    Don't count constant like ops as having a size

--- src/pylir/Optimizer/Analysis/BodySize.cpp
+++ src/pylir/Optimizer/Analysis/BodySize.cpp
@@ -1,6 +1,16 @@
 #include "BodySize.hpp"
 
+#include <mlir/IR/OpDefinition.h>
+
 pylir::BodySize::BodySize(mlir::Operation* operation) : m_size(0)
 {
-    operation->walk([&](mlir::Operation*) { m_size++; });
+    operation->walk(
+        [&](mlir::Operation* op)
+        {
+            if (op->hasTrait<mlir::OpTrait::ConstantLike>())
+            {
+                return;
+            }
+            m_size++;
+        });
 }
