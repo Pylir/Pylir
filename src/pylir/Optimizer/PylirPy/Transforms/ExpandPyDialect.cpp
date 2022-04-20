21f89d02a60d9be17a8193393af191cfac5741b2
zero9178 <markus.boeck02@gmail.com>
2022-04-20 20:41:59 +0200

    Add license and license headers

--- src/pylir/Optimizer/PylirPy/Transforms/ExpandPyDialect.cpp
+++ src/pylir/Optimizer/PylirPy/Transforms/ExpandPyDialect.cpp
@@ -1,3 +1,8 @@
+// Copyright 2022 Markus Böck
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <mlir/Dialect/ControlFlow/IR/ControlFlowOps.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
--- src/pylir/Optimizer/Transforms/Util/SSABuilder.cpp
+++ src/pylir/Optimizer/Transforms/Util/SSABuilder.cpp
@@ -1,3 +1,9 @@
+// Copyright 2022 Markus Böck
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 #include "SSABuilder.hpp"
 
 #include <mlir/Interfaces/ControlFlowInterfaces.h>
