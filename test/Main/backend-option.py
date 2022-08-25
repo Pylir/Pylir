25d64bf54818cef606f3d597414ea6e7f310c7c5
zero9178 <markus.boeck02@gmail.com>
2022-08-25 21:48:53 +0200

    Change test to not trigger a TSAN warning deep within LLVM
    I believe this will be fixed in LLVM soon anyways, but it really does not matter for the purpose of the test and I don't want to have to use a suppression file

--- test/Main/backend-option.py
+++ test/Main/backend-option.py
@@ -1,7 +1,7 @@
-# RUN: pylir %s -c -mllvm -time-passes -o %t 2>&1 | FileCheck %s
-# CHECK: Pass execution timing report
+# RUN: pylir %s -c -mllvm -print-isel-input -o %t 2>&1 | FileCheck %s
+# CHECK: *** Final LLVM Code input to ISel ***
 
 # Check that the backend also passes it to LLD
-# RUN: pylir %s -mllvm -time-passes -o %t -v -### 2>&1 \
+# RUN: pylir %s -mllvm -print-isel-input -o %t -v -### 2>&1 \
 # RUN: | FileCheck %s --check-prefix=LLD
-# LLD: {{(\/mllvm:|--mllvm=)}}-time-passes
+# LLD: {{(\/mllvm:|--mllvm=)}}-print-isel-input
