9f2a017ef065e8aed035a8bb8d8fcab650d9aa8f
zero9178 <markus.boeck02@gmail.com>
2022-08-26 15:22:30 +0200

    [Runtime] Manually apply platform-specific global name mangling in `asm` statements.

--- src/pylir/Runtime/Builtins.hpp
+++ src/pylir/Runtime/Builtins.hpp
@@ -20,7 +20,14 @@ class PyBaseException;
 
 namespace Builtins
 {
-#define BUILTIN(name, symbol, _, Type, ...) extern Type name asm(symbol);
+// 'asm' directive takes the name very literally, going as far as ignoring the platform mangling. We'll therefore have
+// to do it manually.
+#if defined(__APPLE__) || (defined(__i386__) && defined(_WIN32))
+    #define MANGLING "_"
+#else
+    #define MANGLING ""
+#endif
+#define BUILTIN(name, symbol, _, Type, ...) extern Type name asm(MANGLING symbol);
 #include <pylir/Interfaces/BuiltinsModule.def>
 } // namespace Builtins
 
