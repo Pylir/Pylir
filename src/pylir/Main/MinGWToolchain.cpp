6ec52b054ef4960931ead397557e9c31d80c214d
zero9178 <markus.boeck02@gmail.com>
2022-09-10 23:05:12 +0200

    Update license headers
    Remove copyright notice just like LLVM. Having my name as the sole copyright owner will be a lie the moment someone else contributes, and I do not want to take credit for their work.

--- src/pylir/Main/MinGWToolchain.cpp
+++ src/pylir/Main/MinGWToolchain.cpp
@@ -1,9 +1,7 @@
 
-// Copyright 2022 Markus Böck
-//
-// Licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//  Licensed under the Apache License v2.0 with LLVM Exceptions.
+//  See https://llvm.org/LICENSE.txt for license information.
+//  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "MinGWToolchain.hpp"
 
--- src/pylir/Optimizer/PylirPy/IR/PylirPyDialect.cpp
+++ src/pylir/Optimizer/PylirPy/IR/PylirPyDialect.cpp
@@ -1,8 +1,6 @@
-// Copyright 2022 Markus Böck
-//
-// Licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//  Licensed under the Apache License v2.0 with LLVM Exceptions.
+//  See https://llvm.org/LICENSE.txt for license information.
+//  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "PylirPyDialect.hpp"
 
