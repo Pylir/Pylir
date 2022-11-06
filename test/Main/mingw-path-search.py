9c065082aea4c004cc8582941ed99b569593e2c1
zero9178 <markus.boeck02@gmail.com>
2022-11-06 23:35:28 +0100

    [Main] Replace GCC with clang installation autodetection in MinGW
    Since using Clang is the only supported and tested configuration for
    using MinGW, switch the auto detection to searching for a clang
    installation instead of a GCC installation.

---
+++ test/Main/mingw-path-search.py
@@ -0,0 +1,11 @@
+# REQUIRES: system-windows
+
+# RUN: env PATH="%S/Inputs/mingw-single-target-sysroot/bin%{pathsep}%{PATH}" pylir %s -### --target=x86_64-w64-windows-gnu 2>&1 | FileCheck %s
+
+# CHECK: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}lib{{[/\\]+}}clang{{[/\\]+}}99.0.0{{[/\\]+}}lib{{[/\\]+}}windows{{[[:space:]]}}
+# CHECK: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}lib{{[[:space:]]}}
+# CHECK: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}x86_64-w64-mingw32{{[[:space:]]}}
+# CHECK: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}x86_64-w64-windows-gnu{{[[:space:]]}}
+
+# CHECK: -l{{[[:space:]]*}}clang_rt.builtins-x86_64{{[[:space:]]}}
+
---
+++ test/Main/mingw-sysroot.py
@@ -0,0 +1,16 @@
+# RUN: pylir %s --sysroot=%S/Inputs/mingw-single-target-sysroot -### --target=x86_64-w64-windows-gnu 2>&1 | FileCheck %s --check-prefix=SINGLE
+# RUN: pylir %s --sysroot=%S/Inputs/mingw-per-target-sysroot -### --target=x86_64-w64-windows-gnu 2>&1 | FileCheck %s --check-prefix=PER_TARGET
+
+# SINGLE: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}lib{{[/\\]+}}clang{{[/\\]+}}99.0.0{{[/\\]+}}lib{{[/\\]+}}windows{{[[:space:]]}}
+# SINGLE: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}lib{{[[:space:]]}}
+# SINGLE: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}x86_64-w64-mingw32{{[[:space:]]}}
+# SINGLE: -L{{.*}}mingw-single-target-sysroot{{[/\\]+}}x86_64-w64-windows-gnu{{[[:space:]]}}
+
+# SINGLE: -l{{[[:space:]]*}}clang_rt.builtins-x86_64{{[[:space:]]}}
+
+# PER_TARGET: -L{{.*}}mingw-per-target-sysroot{{[/\\]+}}lib{{[/\\]+}}clang{{[/\\]+}}15.0.4{{[/\\]+}}lib{{[/\\]+}}x86_64-w64-windows-gnu{{[[:space:]]}}
+# PER_TARGET: -L{{.*}}mingw-per-target-sysroot{{[/\\]+}}lib{{[[:space:]]}}
+# PER_TARGET: -L{{.*}}mingw-per-target-sysroot{{[/\\]+}}x86_64-w64-mingw32{{[[:space:]]}}
+# PER_TARGET: -L{{.*}}mingw-per-target-sysroot{{[/\\]+}}x86_64-w64-windows-gnu{{[[:space:]]}}
+
+# PER_TARGET: -l{{[[:space:]]*}}clang_rt.builtins{{[[:space:]]}}
