22df99a8b3134235d84a4425118b8724b56821a8
zero9178 <markus.boeck02@gmail.com>
2023-02-13 23:10:11 +0100

    Fix clang-tidy warning from last commit and add doc commennts

--- src/pylir/Optimizer/PylirPy/Transforms/Util/InlinerUtil.hpp
+++ src/pylir/Optimizer/PylirPy/Transforms/Util/InlinerUtil.hpp
@@ -9,11 +9,22 @@
 namespace pylir::Py
 {
 
+/// Simple struct returning all ops that were inlined from a use of 'inlineCall'.
 struct InlinedOps
 {
-    mlir::Operation* firstOperationInFirstBlock;
-    mlir::Region::iterator endBlock;
+    /// The first operation of the callable that was inlined.
+    mlir::Operation* firstOperationInFirstBlock{};
+    /// End iterator of the range of blocks that were inlined.
+    /// In other words, one past the last block that was inlined.
+    mlir::Region::iterator endBlock{};
 };
 
+/// Inlines 'callable' into 'call' and replaces it. 'call' is assumed to resolve to 'callable' and have matching
+/// argument count and type as the callable region of 'callable'.
+/// Returns a struct allowing iteration of the inlined ops with 'call's parent region.
+///
+/// Note: This is required over MLIRs version due to non-trivial deficiencies in MLIRs inliner interface
+/// (that we should probably fix). In particular, this implementation properly handles inlining of exception handling
+/// ops.
 InlinedOps inlineCall(mlir::CallOpInterface call, mlir::CallableOpInterface callable);
 } // namespace pylir::Py
