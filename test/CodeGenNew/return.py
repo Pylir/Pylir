5997c27dc84919599e261c6d04432269cc95b7a9
Markus Böck <markus.boeck02@gmail.com>
2024-02-08 18:35:29 +0100

    [CodeGenNew] Implement `return` statements (#109)
    Return statements return the value of the given expression or `None` if
    no expression is present. Not part of this PR is the execution of
    finally blocks which is deferred to when `finally` is implemented.

---
+++ test/CodeGenNew/return.py
@@ -0,0 +1,18 @@
+# RUN: pylir %s -Xnew-codegen -emit-pylir -o - -S | FileCheck %s
+
+# CHECK: #[[$NONE:.*]] = #py.globalValue<builtins.None{{(,|>)}}
+
+def foo():
+    return 3
+
+
+# CHECK-LABEL: func "__main__.foo"
+# CHECK: %[[THREE:.*]] = py.constant(#py.int<3>)
+# CHECK: return %[[THREE]]
+
+def bar():
+    return
+
+# CHECK-LABEL: func "__main__.bar"
+# CHECK: %[[NONE:.*]] = py.constant(#[[$NONE]])
+# CHECK: return %[[NONE]]
