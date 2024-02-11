e04c9d41bc169da074bbcde05fa37d0f8f749112
Markus Böck <markus.boeck02@gmail.com>
2024-02-11 22:29:51 +0100

    [CodeGenNew] Implement intrinsic calls (#119)
    Intrinsic calls are a very important part of the builtins implementation
    as they make it possible to directly create MLIR implementations
    implementing semantics that are impossible in pure python code. The
    kinds of intrinsics are all automatically generated using TableGen from
    the `py` dialect.
    
    This PR hooks these intrinsics up to the new codegen. The code is mostly
    taken from the old but adjusted for the evolved style.

---
+++ test/CodeGenNew/intrinsics.py
@@ -0,0 +1,31 @@
+# RUN: pylir %s -Xnew-codegen -emit-pylir -o - -S | FileCheck %s
+
+# CHECK-LABEL: init "__main__"
+# CHECK: %[[DICT:.*]] = py.makeDict ()
+
+def foo():
+    pass
+
+
+# CHECK: py.dict_setItem
+# CHECK: %[[STR:.*]] = py.constant(#py.str<"foo">)
+# CHECK: %[[HASH:.*]] = py.str_hash %[[STR]]
+# CHECK: %[[FOO:.*]] = py.dict_tryGetItem %[[DICT]][%[[STR]] hash(%[[HASH]])]
+# CHECK: %[[T:.*]] = py.typeOf %[[FOO]]
+# CHECK: %[[STR:.*]] = py.constant(#py.str<"t">)
+# CHECK: %[[HASH:.*]] = py.str_hash %[[STR]]
+# CHECK: py.dict_setItem %[[DICT]][%[[STR]] hash(%[[HASH]])] to %[[T]]
+t = pylir.intr.typeOf(foo)
+
+# CHECK: %[[ZERO:.*]] = py.constant(#py.int<0>)
+# CHECK: %[[ONE:.*]] = py.constant(#py.int<1>)
+# CHECK: %[[CMP:.*]] = py.int_cmp ne %[[ZERO]], %[[ONE]]
+# CHECK: %[[BOOL:.*]] = py.bool_fromI1 %[[CMP]]
+pylir.intr.int.cmp("ne", 0, 1)
+
+# CHECK: %[[ZERO:.*]] = py.constant(#py.int<0>)
+# CHECK: %[[ONE:.*]] = py.constant(#py.int<1>)
+# CHECK: %[[TWO:.*]] = py.constant(#py.int<2>)
+# CHECK: %[[THREE:.*]] = py.constant(#py.int<3>)
+# CHECK: py.function_call %[[ZERO]](%[[ONE]], %[[TWO]], %[[THREE]])
+pylir.intr.function.call(0, 1, 2, 3)
