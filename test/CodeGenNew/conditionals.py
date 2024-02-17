c05f1eb661386171611bcee70845dc3418af53f2
Markus Böck <markus.boeck02@gmail.com>
2024-02-17 09:52:00 +0100

    [CodeGenNew] Implement conditional expressions

---
+++ test/CodeGenNew/conditionals.py
@@ -0,0 +1,23 @@
+# RUN: pylir %s -Xnew-codegen -emit-pylir -o - -S | FileCheck %s
+
+# CHECK: #[[$BOOL:.*]] = #py.globalValue<builtins.bool{{,|>}}
+
+# CHECK-LABEL: init "__main__"
+
+# CHECK: %[[ZERO:.*]] = py.constant(#py.int<0>)
+# CHECK: %[[BOOL:.*]] = py.constant(#[[$BOOL]])
+# CHECK: %[[B:.*]] = call %[[BOOL]](%[[ZERO]])
+# CHECK: %[[I1:.*]] = py.bool_toI1 %[[B]]
+# CHECK: cf.cond_br %[[I1]], ^[[BB1:.*]], ^[[BB2:[[:alnum:]]+]]
+
+# CHECK: ^[[BB1]]:
+# CHECK: %[[FIVE:.*]] = py.constant(#py.int<5>)
+# CHECK: cf.br ^[[BB3:.*]](%[[FIVE]] : !py.dynamic)
+
+# CHECK: ^[[BB2]]:
+# CHECK: %[[THREE:.*]] = py.constant(#py.int<3>)
+# CHECK: cf.br ^[[BB3]](%[[THREE]] : !py.dynamic)
+
+# CHECK: ^[[BB3]](%[[VALUE:[[:alnum:]]+]]: !py.dynamic loc({{.*}}):
+# CHECK: py.dict_setItem %{{.*}}[%{{.*}}] to %[[VALUE]]
+x = 5 if 0 else 3
