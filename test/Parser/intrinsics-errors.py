a1a4cbfd23bb826eec8c1e8710d83ba96ea4f992
Markus Böck <markus.boeck02@gmail.com>
2024-02-20 18:02:12 +0100

    add forgotten test

--- test/Parser/intrinsics-errors.py
+++ test/Parser/intrinsics-errors.py
@@ -46,3 +46,25 @@ class Foo(*[]):
 # expected-error@below {{expected constant expression}}
 class Foo([]):
     pass
+
+
+@pylir.intr.const_export
+class Foo:
+    # expected-error@below {{only single assignments and function definitions allowed in 'const_export' class}}
+    a, b = 1, 2
+
+    # expected-error@below {{only single assignments and function definitions allowed in 'const_export' class}}
+    a += 3
+
+    # expected-error@below {{only single assignments and function definitions allowed in 'const_export' class}}
+    if True:
+        pass
+
+    # expected-error@below {{only single assignments and function definitions allowed in 'const_export' class}}
+    a + b
+
+    # expected-error@below {{expected constant expression}}
+    a = b
+
+    valid = ("a", "b")
+    valid = pylir.intr.type.__slots__
