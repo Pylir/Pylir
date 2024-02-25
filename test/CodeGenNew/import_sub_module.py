9bb40d125510522eb86beb182e0ee82200f69bd7
Markus Böck <markus.boeck02@gmail.com>
2024-02-25 08:05:18 +0100

    [CodeGenNew] Partially implement `import <module>`
    Like the old codegen, the import functionality triggers initialization of the module which for now is required for the builtin module. It does not yet implement initialization of the module object or any name binding. The logic is mostly taken from the old codegen with the expectation of it being removed very soon.

---
+++ test/CodeGenNew/import_sub_module.py
@@ -0,0 +1,29 @@
+#  // Licensed under the Apache License v2.0 with LLVM Exceptions.
+#  // See https://llvm.org/LICENSE.txt for license information.
+#  // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# RUN: split-file %s %t
+# RUN: pylir -Xnew-codegen %t/main.py -I %t/wrong -I %t/right -S -emit-pylir -o - | FileCheck %s
+
+#--- main.py
+
+import foo.bar.baz
+
+# CHECK: initModule @foo
+# CHECK: initModule @foo.bar
+# CHECK: initModule @foo.bar.baz
+
+#--- right/foo/__init__.py
+
+#--- right/foo/bar/__init__.py
+
+#--- right/foo/bar/baz.py
+
+print(True)
+
+# CHECK: init "foo.bar.baz"
+# CHECK: constant(#py.bool<True>)
+
+#--- wrong/foo/bar/baz.py
+
+print(False)
