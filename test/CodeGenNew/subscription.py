87d856300e782891090be4001fb17beec375ab51
Markus Böck <markus.boeck02@gmail.com>
2024-03-01 18:53:31 +0100

    [CodeGenNew][PyHIR] Implement subscription operator (#156)
    This PR implements python subscription operators, both assignment and
    reading, by lowering them to `getItem` and `setItem` ops of the `pyHIR`
    dialect, also introduced with this PR. A more general `specialMethod` op
    was considered instead of `getItem` and `setItem` but specific ops for
    subscription were chosen instead for better syntax and assigning proper
    meaning to each operand.

---
+++ test/CodeGenNew/subscription.py
@@ -0,0 +1,10 @@
+# RUN: pylir %s -Xnew-codegen -emit-pylir -o - -c -S | FileCheck %s
+
+# CHECK-LABEL: func "__main__.test"
+# CHECK-SAME: %[[A:[[:alnum:]]+]]
+# CHECK-SAME: %[[B:[[:alnum:]]+]]
+# CHECK-SAME: %[[C:[[:alnum:]]+]]
+def test(a, b, c):
+    # CHECK: %[[A_C:.*]] = getItem %[[A]][%[[C]]]
+    # CHECK: setItem %[[A]][%[[B]]] to %[[A_C]]
+    a[b] = a[c]
