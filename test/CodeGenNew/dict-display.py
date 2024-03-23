4d8344871a5802e97a6ad93b45d87803a7c87e10
Markus Böck <markus.boeck02@gmail.com>
2024-03-23 13:32:39 +0000

    [CodeGenNew] Implement basic dictionary displays
    This PR implements dictionary displays that are a list of items with optional expansions. It does not yet implement dictionary comprehensions.

---
+++ test/CodeGenNew/dict-display.py
@@ -0,0 +1,21 @@
+# RUN: pylir %s -Xnew-codegen -emit-pylir -o - -S | FileCheck %s
+
+# CHECK: #[[$HASH:.*]] = #py.globalValue<builtins.hash{{,|>}}
+
+# CHECK-LABEL: init "__main__"
+
+# CHECK: py.makeDict ()
+{}
+
+
+# CHECK-LABEL: func "__main__.foo"
+# CHECK-SAME: %[[X:[[:alnum:]]+]]
+def foo(x):
+    # CHECK: %[[SEVEN:.*]] = py.constant(#py.int<7>)
+    # CHECK: %[[EIGHT:.*]] = py.constant(#py.int<8>)
+    # CHECK: %[[HASH_REF:.*]] = py.constant(#[[$HASH]])
+    # CHECK: %[[HASH:.*]] = call %[[HASH_REF]](%[[SEVEN]])
+    # CHECK: %[[INT:.*]] = py.int_toIndex %[[HASH]]
+    # CHECK: py.makeDict (%[[SEVEN]] hash(%[[INT]]) : %[[EIGHT]], **%[[X]])
+    return {7: 8, **x}
+
