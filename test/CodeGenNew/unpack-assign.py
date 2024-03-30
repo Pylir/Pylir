23ff2a1fb64b793882aa06a9b3c72ecd7f03b219
Markus Böck <markus.boeck02@gmail.com>
2024-03-30 15:45:43 +0100

    [CodeGenNew] Implement iterable unpacking
    The `py` dialect already contains an existing operation to perform this operation. We will probably want to move this operation to the `HIR` dialect in the future as that is the more fitting abstraction level. Until then, the implementation for iterable packing is the same as in the old CodeGen.

---
+++ test/CodeGenNew/unpack-assign.py
@@ -0,0 +1,14 @@
+# RUN: pylir %s -Xnew-codegen -emit-pylir -o - -S | FileCheck %s
+
+
+# CHECK-LABEL: func "__main__.test"
+# CHECK-SAME: %[[ITERABLE:[[:alnum:]]+]]
+def test(iterable):
+    # CHECK: %[[AB:[[:alnum:]]+]]:2 = py.unpack %[[ITERABLE]] : (!py.dynamic, !py.dynamic)
+    a, b = iterable
+    # CHECK: %[[A:.*]], %[[B:.*]] = py.unpack %[[AB]]#0 : (), !py.dynamic, (!py.dynamic)
+    *a, b = a
+    # CHECK: %[[A:.*]], %[[B_2:.*]], %[[C:.*]] = py.unpack %[[B]] : (!py.dynamic), !py.dynamic, (!py.dynamic)
+    a, *b, c = b
+    # CHECK: %[[A:.*]], %[[B:.*]] = py.unpack %[[C]] : (!py.dynamic), !py.dynamic
+    a, *b = c
