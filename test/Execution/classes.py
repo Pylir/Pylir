fa7a489130d8d09ca87efbfb07502d48ed73a3fa
Markus Böck <markus.boeck02@gmail.com>
2024-05-26 10:19:11 +0200

    Remove accidental license header

--- test/Execution/classes.py
+++ test/Execution/classes.py
@@ -1,11 +1,3 @@
-#  Licensed under the Apache License v2.0 with LLVM Exceptions.
-#  See https://llvm.org/LICENSE.txt for license information.
-#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#  Licensed under the Apache License v2.0 with LLVM Exceptions.
-#  See https://llvm.org/LICENSE.txt for license information.
-#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
 # RUN: pylir %s -o %t
 # RUN: %t | FileCheck %s
 
